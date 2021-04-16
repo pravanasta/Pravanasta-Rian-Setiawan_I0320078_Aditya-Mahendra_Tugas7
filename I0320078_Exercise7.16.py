29bcc2f4c5dd707125f129f0736960aa6b6cfcaa
Pravanasta Rian <pravanastarian@gmail.com>
2021-04-16 13:58:14 +0700

    Tugas Praktikum Programa Komputer7

---
+++ I0320078_Exercise7.16.py
@@ -0,0 +1,7 @@
+a = 100.63
+b = 125.23
+import math
+print("a= ", a)
+print("b= ", b)
+print("\nmath a cell: ", math.ceil(a))
+print("math b cell: ", math.ceil(b))
---
+++ I0320078_Exercise7.8.py
@@ -0,0 +1,3 @@
+str = "hello world"
+s = str.center(20,'x')
+print(s)
